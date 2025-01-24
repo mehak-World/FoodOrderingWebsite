cba5db6d19581690d664392e6c911188250a7abd
Mehak Narang <139652649+mehak-World@users.noreply.github.com>
2025-01-23 20:45:49 -0800

    Update useFetchResMenu.js

--- src/utils/useFetchResMenu.js
+++ src/utils/useFetchResMenu.js
@@ -7,7 +7,7 @@ const useFetchResMenu = (restId) => {
       const fetchData = async () => {
         try {
           const response = await fetch(
-            `https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.6425496&lng=76.8173359&restaurantId=${restId}`
+            `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.6425496&lng=76.8173359&restaurantId=${restId}`
           );
           const data = await response.json();
           setMenu(data);
